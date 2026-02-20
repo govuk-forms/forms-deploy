4e2f00e581c17afb1d25d9d429c26539f7b50618
Sean Rankine <sean.rankine@me.com>
2026-02-20 12:04:10 +0000

    Add common role to run ECS Scheduled Events
    This is a shared IAM role for ECS events, this is needed by EventBridge to run ECS scheduled tasks.

--- infra/modules/environment/ecs.tf
+++ infra/modules/environment/ecs.tf
@@ -6,3 +6,29 @@ resource "aws_ecs_cluster" "forms" {
     value = "enhanced"
   }
 }
+
+
+## ECS Events Role
+## This a common role used by EventBridge to run ECS tasks. Only needs to be created once per account.
+
+data "aws_iam_policy_document" "ecs_events_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_events" {
+  name               = "ecsEventsRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_events_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_events_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+  role       = aws_iam_role.ecs_events.name
+}
